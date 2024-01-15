1fc4811063a51174398caf221534b270431a53cb
Никола "hauzer" Вукосављевић <hauzer.nv@gmail.com>
2024-01-16 00:59:33 +0100

    Update versions and description

--- init.lua
+++ init.lua
@@ -3,13 +3,13 @@
 for_module = 'tome'
 long_name = 'Resourceful Wanderers'
 short_name = 'resourceful-wanderers'
-version = {1,7,4}
-addon_version = {1,1,1}
+version = {1,7,6}
+addon_version = {2,0,0}
 weight = 100
 
 author = {'hauzer', 'hauzer.nv@gmail.com'}
 homepage = 'https://forums.te4.org/viewtopic.php?f=50&t=52893'
-description = [[Help Wanderers make use of trees which are otherwise hard or impossible to use due to insufficient or non-existent resource generation.]]
+description = [[Help Wanderers make use of trees which are otherwise hard or impossible to use.]]
 tags = {
     'randventurer', 'wanderer', 'wanderers', 'resource', 'resources', 'steam', 'equilibrium',
     'souls', 'soul', 'insanity', 'generation', 'steamtech', 'wild gift', 'wildgift'
