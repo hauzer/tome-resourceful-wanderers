64b25019c88f59faec5b97d6be16a9c73f53078d
Никола "hauzer" Вукосављевић <hauzer.nv@gmail.com>
2024-01-15 19:43:48 +0100

    Add unarmed-mastery and combos areas

--- superload/mod/dialogs/WandererSeed.lua
+++ superload/mod/dialogs/WandererSeed.lua
@@ -298,7 +298,7 @@ function _M:setup_resourceful_wanderers()
                     }
                 }
             },
-            ['steamtech-crafting'] = {
+            steamtech = {
                 addon = 'orcs',
                 cover_category = 'steamtech',
                 cover_talent_types = {
@@ -924,6 +924,72 @@ function _M:setup_resourceful_wanderers()
                         _t'Sword and sorcery!',
                         _t'Why not both?',
                         _t'Where are the naysayers who told me I should specialize?'
+                    }
+                }
+            },
+            ['unarmed-mastery'] = {
+                cover_talent_types = {
+                    'technique/pugilism',
+                    'technique/finishing-moves',
+                    'technique/grappling',
+                    'technique/unarmed-discipline'
+                },
+                talent_type = {
+                    names = {
+                        'monk',
+                        'karatist',
+                        'disciple'
+                    },
+                    is_generic = true,
+                    talents = {
+                        {
+                            id = 'T_UNARMED_MASTERY',
+                            is_signature = true
+                        },
+                        'T_UNIFIED_BODY',
+                        'T_HEIGHTENED_REFLEXES',
+                        'T_REFLEX_DEFENSE'
+                    },
+                    max_talents = 3,
+                    descriptions = {
+                        _t'In a healthy body, a healthy mind.',
+                        _t'Relax your muscles, feel the flow around you.',
+                        _t'Misplaced strength is as dangerous as any opponent.'
+                    }
+                }
+            },
+            combos = {
+                cover_talent_types = {
+                    'technique/finishing-moves',
+                    'technique/grappling',
+                    'technique/unarmed-discipline'
+                },
+                talent_type = {
+                    names = {
+                        'bruiser',
+                        'ringster',
+                        'boxer'
+                    },
+                    talents = {
+                        {
+                            id = 'T_DOUBLE_STRIKE',
+                            is_signature = true
+                        },
+                        'T_SPINNING_BACKHAND',
+                        'T_AXE_KICK',
+                        'T_FLURRY_OF_FISTS',
+                        'T_UPPERCUT',
+                        'T_CONCUSSIVE_PUNCH',
+                        'T_BUTTERFLY_KICK',
+                        'T_HAYMAKER',
+                        'T_PUSH_KICK',
+                        'T_RELENTLESS_STRIKES'
+                    },
+                    max_talents = 4,
+                    descriptions = {
+                        _t'One, two, three! That\'s how you do it.',
+                        _t'I didn\'t see his left hook coming...',
+                        _t'I need to train!'
                     }
                 }
             }
